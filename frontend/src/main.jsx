1020608960318864332ede60bd2ac1e54187d6f8
Pablo Fuset Sanz <pablofuset@gmail.com>
2025-08-20 12:29:25 +0200

    v3.1 - chore: set up initial Vite project structure

---
+++ frontend/src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// We will create App.jsx and index.css later
+// import App from './App.jsx';
+// import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <h1>Hello, World!</h1>
+  </React.StrictMode>,
+)
